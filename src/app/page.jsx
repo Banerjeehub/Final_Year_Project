f43aaaddb8d2983a200ef29ea090a7e5999581af
Padmanava_Banerjee <padmanavabanerjee2016@gmail.com>
2023-11-25 19:55:42 +0530

    Base

---
+++ src/app/page.jsx
@@ -0,0 +1,57 @@
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+        necessitatibus ut quaerat possimus minus ea obcaecati eveniet natus.
+        Recusandae nesciunt similique sint consequatur, a omnis.
+      </h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam vero
+      error et possimus minus ullam rerum odit quibusdam esse. Facilis molestiae
+      et sunt ipsam. Quia ratione, doloribus cumque hic, quis repudiandae vero
+      reprehenderit voluptates illo natus nisi! Fugit quos quia unde sunt,
+      aliquid ad fuga quod deleniti veritatis omnis consectetur beatae sequi
+      provident recusandae, vitae velit reiciendis blanditiis commodi
+      exercitationem pariatur temporibus ducimus facere quibusdam modi.
+      Distinctio officiis ab esse sapiente enim dolorem reiciendis, impedit hic!
+      Dolores soluta ea debitis, deleniti sit impedit aliquam nulla! Iste
+      excepturi voluptate quod eum necessitatibus asperiores sed debitis officia
+      perferendis ducimus voluptas est rerum dolorum ipsum, consectetur
+      aspernatur? In tempora similique aliquam atque dolor assumenda minima
+      nobis quae impedit neque voluptatem molestiae, dolores nisi iusto et id
+      ratione laudantium doloribus. Voluptatibus excepturi reprehenderit
+      assumenda sint, qui provident ratione fugiat consequatur, aspernatur
+      incidunt nemo officia quisquam repellendus libero nesciunt accusamus quod
+      tenetur! Dolore esse beatae sunt, labore tenetur odio excepturi dicta,
+      voluptate delectus natus rerum deleniti nemo ipsam, in facere porro
+      possimus. Vitae autem, voluptatibus molestias, facere ratione voluptatem
+      consequatur veniam accusantium non tempore sed nesciunt voluptates fugiat
+      necessitatibus doloremque ipsum labore. Iste officia, quibusdam
+      consequatur amet vel a, reprehenderit sapiente, vitae atque fugit
+      incidunt. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque
+      quis officia vitae debitis odit laborum, tenetur inventore assumenda fuga
+      minima laudantium molestias quas quibusdam voluptatem dolorem quasi magnam
+      eum illo! Asperiores nam et facere at exercitationem voluptas? Dolorem,
+      eaque autem dolores aperiam exercitationem, laborum, sapiente fuga numquam
+      repudiandae officia magnam? Odit laboriosam, provident tempore dignissimos
+      ex ab autem eveniet. Saepe architecto non deserunt dolor voluptas? Odit
+      ipsum nemo veniam, reiciendis iste pariatur sit ipsam inventore totam
+      obcaecati quidem. Ullam, facilis? Ipsum veritatis minima inventore
+      suscipit deserunt sint alias at vero, recusandae aspernatur rem excepturi
+      cupiditate ipsam. Aliquam minima aspernatur nostrum consectetur quas quod
+      doloremque ea veniam. Harum eum, cupiditate sequi optio incidunt quibusdam
+      cumque aut soluta ducimus voluptas eligendi distinctio explicabo
+      blanditiis accusantium amet, eos dicta repudiandae quasi mollitia modi
+      tenetur dolor nesciunt perferendis quam? Voluptatum sunt provident quos a
+      impedit ipsam magni repellat earum nihil necessitatibus nemo ea assumenda
+      voluptas consectetur fugiat debitis, adipisci vel ullam inventore modi est
+      temporibus nulla asperiores ipsa! Porro, deserunt! Qui nulla blanditiis at
+      necessitatibus saepe. Eum voluptate accusamus tenetur dolorum fugit quod
+      suscipit delectus reprehenderit provident, aut ipsam distinctio ad
+      doloremque totam repudiandae quasi autem enim. Voluptas, praesentium ea
+      omnis ad sed alias.
+    </div>
+  );
+}
